d2d2f57b42c457a69bc3e539b5a5646669fca701
whatisbyandby <whatisbyandby@gmail.com>
2019-06-20 22:52:59 -0600

    Changed to using a context based widget

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -3,10 +3,12 @@ import "../ui/style.css";
 
 export interface HelloProps {
     message: string;
+    attributeMessage: string | number | boolean | null;
 }
 
 export const Hello: React.SFC<HelloProps> = (props) => (
     <div className="main-div">
         <h1>{props.message}</h1>
+        <h3>{props.attributeMessage}</h3>
     </div>
 );
--- src/components/HelloWorldContainer.tsx
+++ src/components/HelloWorldContainer.tsx
@@ -3,9 +3,11 @@ import { Hello } from "./Hello";
 
 interface HelloWorldContainerState {
     displayMessage: string;
+    messageAttribute: string | number | boolean | null;
 }
 
 interface WrapperProps {
+    mxObject?: mendix.lib.MxObject;
     class?: string;
     mxform: mxui.lib.form._FormBase;
     style?: string;
@@ -13,24 +15,69 @@ interface WrapperProps {
 
 interface HelloWorldContainerProps extends WrapperProps {
     displayMessage: string;
+    messageAttribute: string | null;
 }
 
 class HelloWorldContainer extends React.Component<HelloWorldContainerProps, HelloWorldContainerState> {
+    private subscriptionHandles: number[];
+
     constructor(props: HelloWorldContainerProps) {
         super(props);
+
+        this.subscriptionHandles = [];
+        this.state = this.updateState(props.mxObject);
+        this.subscriptionCallback = this.subscriptionCallback.bind(this);
+
         this.state = {
-           displayMessage: ""
+           displayMessage: "",
+           messageAttribute: ""
         };
     }
 
-    componentDidMount() {
-        this.setState({ displayMessage: this.props.displayMessage });
+    componentWillReceiveProps(newProps: HelloWorldContainerProps) {
+        this.resetSubscriptions(newProps.mxObject);
+        this.setState(this.updateState(newProps.mxObject));
+    }
+
+    componentWillUnmount() {
+        this.subscriptionHandles.forEach(mx.data.unsubscribe);
+    }
+
+    private resetSubscriptions(mxObject?: mendix.lib.MxObject) {
+        this.subscriptionHandles.forEach(mx.data.unsubscribe);
+        this.subscriptionHandles = [];
+
+        if (mxObject) {
+            this.subscriptionHandles.push(mx.data.subscribe({
+                callback: this.subscriptionCallback,
+                guid: mxObject.getGuid()
+            }));
+        }
+    }
+
+    private updateState(mxObject = this.props.mxObject): HelloWorldContainerState {
+        return {
+            messageAttribute: this.getAttributeValue(this.props.messageAttribute, mxObject),
+            displayMessage: this.props.displayMessage
+        };
+    }
+
+    private subscriptionCallback() {
+        this.setState(this.updateState());
+    }
+
+    private getAttributeValue(attribute: string | null, mxObject?: mendix.lib.MxObject): string {
+        if (mxObject && attribute) {
+            return mxObject.get(attribute) as string;
+        }
+        return "";
     }
 
     render() {
-        return(
+         return(
             <Hello
                 message={this.state.displayMessage}
+                attributeMessage={this.state.messageAttribute}
             />
         );
     }
